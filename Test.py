c8468d6795e6998c06be9582c292c2c8b84e9218
wxynly <2511985806@qq.com>
2019-04-03 08:43:10 +0800

    add 1111.md

---
+++ Test.py
@@ -0,0 +1,12 @@
+# print(3)
+# x=44
+# print(x)
+# for i in range(10):
+#     print(i)
+# import numpy
+# x=numpy.ones(3)
+# print(x)
+# print(bytes('王鑫','gbk'))
+# print(int('1'*200,2))
+x=input('please input:')
+print(x)
